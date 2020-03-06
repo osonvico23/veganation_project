edb88b12cfaf18ac3ab4cc717407c9403f63557e
“Orla <2391647s@student.gla.ac.uk>
2020-03-06 15:11:48 +0000

    updates

--- veganation/urls.py
+++ veganation/urls.py
@@ -5,7 +5,6 @@ app_name = 'veganation'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.signup, name ='signup'),
     path('restaurants/', views.restaurants, name='restaurants'),
     path('protests/', views.protests, name='protests'),
     path('signup/', views.signup, name='signup')
