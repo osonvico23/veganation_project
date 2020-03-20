b1abab92a4a79a7d18476c01715130bb338e7135
“Kamilla <2394548k@student.gla.ac.uk>
2020-03-20 14:31:58 +0000

    url fixed

--- veganation_project/urls.py
+++ veganation_project/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('veganation/', include('veganation.urls')),
     path('admin/', admin.site.urls),
-    url(r'^ratings/', include('star_ratings.urls', namespace='ratings', app_name='ratings')),
+    path('ratings/', include('star_ratings.urls', namespace='ratings'), name='ratings'),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
