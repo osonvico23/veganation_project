208d74f7e581cc6cd56f9f5be577821688f87b04
Marzia Deodato <2391580d@student.gla.ac.uk>
2020-03-28 12:30:42 +0100

    populating forms with current users' info

--- veganation/views.py
+++ veganation/views.py
@@ -7,6 +7,7 @@ from veganation.forms import UserForm, UserProfileForm, ProfileUpdateForm
 from django.contrib.auth import authenticate, login
 from django.urls import reverse
 from django.shortcuts import render_to_response
+from django.contrib import messages
 
 def index(request):
     return render(request, 'veganation/index.html')
@@ -19,9 +20,21 @@ def protests(request):
     context_dict = {}
     return render(request, 'veganation/protests.html', context=context_dict)
 
-def myaccount(request):
-    u_form = UserProfileForm()
-    p_form = ProfileUpdateForm()
+def myaccount(request): 
+    if request.method == 'POST':
+        u_form = UserProfileForm(request.POST, instance= request.user)
+        p_form = ProfileUpdateForm(request.POST, 
+                                   request.FILES, 
+                                   instance = request.user.myaccount)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('myaccount')
+    else:
+        u_form = UserProfileForm(instance= request.user)
+        p_form = ProfileUpdateForm(instance = request.user.myaccount)
+
 
     context_dict = {
         'u_form' : u_form,
