4560b26766e06ef2914e21d01912829acf7d0625
Marzia Deodato <2391580d@student.gla.ac.uk>
2020-03-28 13:08:44 +0100

    automatically resize profile pics when uploading

--- veganation/models.py
+++ veganation/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.timezone import now
+from PIL import Image
 
 from django_google_maps import fields as map_fields
 # Create your models here.
@@ -23,7 +24,20 @@ class UserProfile(models.Model):
     firstName = models.CharField(max_length=30, blank=True)
     lastName = models.CharField(max_length=40, blank=True)
     def __str__(self):
-        return self.user.username
+        return f'{self.user.username} UserProfile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.picture.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
+
+
 
 class Rental(models.Model):
     address = map_fields.AddressField(max_length=200)
