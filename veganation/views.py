e3ca358d3820bb58f0779542c6e2b355be933f3d
Marzia Deodato <marziadeodato@gmail.com>
2020-03-29 12:29:55 +0200

    My Account appears when user is logged in on top right corner

--- veganation/views.py
+++ veganation/views.py
@@ -88,8 +88,8 @@ def signup(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}! Now you can login from the top right corner Login Here!')
-            return redirect('index')
+            messages.success(request, f'Your account has been created! You are now able to log in!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'veganation/signup.html', {'form': form})
