75363bc5b5a2aaf1aa9fa4b1c41715ab81483d96
Marzia Deodato <marziadeodato@gmail.com>
2020-03-29 15:34:44 +0200

    pics from admin ok, working on uploading from frontend

--- veganation/models.py
+++ veganation/models.py
@@ -24,18 +24,36 @@ class UserProfile(models.Model):
     firstName = models.CharField(max_length=30, blank=True)
     lastName = models.CharField(max_length=40, blank=True)
 
+
+
     def __str__(self):
         return f'{self.user.username} UserProfile'
 
-    def save(self):
-        super().save()
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = UserProfile.objects.create(user = kwargs['instance'])
+post_save.connect(create_profile, sender = User)
 
-        img = Image.open(self.image.path)
+def save(self):
+    super().save()
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    img = Image.open(self.image.path)
+
+    if img.height > 300 or img.width > 300:
+        output_size = (300, 300)
+        img.thumbnail(output_size)
+        img.save(self.image.path)
+
+#@receiver(post_save, sender = User)
+#def create_user_profile(sender, instance, created, **kwargs):
+ #   if created:
+  #      UserProfile.objects.created(user = instance)
+#@receiver(post_save, sender = User)
+#def save_user_profile(sender, instance, **kwargs):
+ #   instance.profile.save()
+
+
+
 
 class Rental(models.Model):
     address = map_fields.AddressField(max_length=200)
