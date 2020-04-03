9b1bb26df529935546261a58a79adfca075e5093
Marzia Deodato <marziadeodato@gmail.com>
2020-04-03 13:37:52 +0200

    first test works- checking UserRegisterForm

--- veganation/tests.py
+++ veganation/tests.py
@@ -4,6 +4,13 @@ from veganation.models import UserProfile
 from django.core.files import File
 from django.utils import timezone
 import os
+import veganation
+from veganation import forms
+
+FAILURE_HEADER = f"{os.linesep}{os.linesep}{os.linesep}================{os.linesep}TwD TEST FAILURE =({os.linesep}================{os.linesep}"
+FAILURE_FOOTER = f"{os.linesep}"
+
+f"{FAILURE_HEADER} {FAILURE_FOOTER}"
 
 #the tests for veganation website.
 
@@ -11,11 +18,36 @@ import os
 
 class MyAccountTest(TestCase):
     def set_up(self):
-        user = User.objects.create(username = 'account1', email = 'account1@account.com', password = 'password123')
+        user = User.objects.create(username = 'account1', email = 'account1@account1.com', password = 'password123')
         user.save()
 
         myaccount = UserProfile.objects.create(user=user)
         imgpath = os.path.join(os.getcwd(), 'media', 'default' + '.jpg')
         myaccount.image.save('default', File(open(imgpath, 'rb')))
         myaccount.save()
+
+    
+    def test_user_register(self):
+
+       
+       #test whether UserRegisterForm matches User Model
+        user_form = forms.UserRegisterForm()
+        self.assertEqual(type(user_form.__dict__['instance']), User, f"{FAILURE_HEADER}Your UserRegisterForm does not match up to the User model. Check your Meta definition of UserRegisterForm and try again.{FAILURE_FOOTER}")
+        
+        #form_data = {
+           # 'username': 'account2',
+            #'email' : 'account2@account2.com',
+            #'password1' : 'password123',
+            #'password2' : 'password123',
+            #'firstName': 'TestfirstName',
+            #'lastName': 'TestlastName',
+            #'gender': 'gender1',
+            #'veganSince' : 'child',
+            #'age' : '23',
+            #'quote' : 'vegan quote example',
+            #'occupation' : 'occupation example',
+            #'city' : 'Glasgow'
+        #}
+        #form = UserRegisterForm(data = form_data)
+        #self.assertTrue(form.is_valid())
 
