c5f28b3a52f0eb760f731c87fbeb8dc3a8cba679
Marzia Deodato <marziadeodato@gmail.com>
2020-04-03 12:53:20 +0200

    working on tests

--- veganation/tests.py
+++ veganation/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from veganation.forms import UserRegisterForm, UserProfileForm, UserUpdateForm, ProfileUpdateForm
+from veganation.models import UserProfile
+from django.core.files import File
+from django.utils import timezone
+import os
 
-# Create your tests here.
+#the tests for veganation website.
+
+#this test checks if a user is set up correctly 
+
+
