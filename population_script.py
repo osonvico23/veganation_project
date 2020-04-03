e96bac6d242d02e48e37ee2cdf6de4cfb5a43d6c
Marzia Deodato <marziadeodato@gmail.com>
2020-04-03 16:29:40 +0200

    completed population_script.py

--- population_script.py
+++ population_script.py
@@ -2,9 +2,10 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'veganation_project.settings')
 import django
 django.setup()
-from veganation.models import Rate, UserProfile
+from veganation.models import Rate, UserProfile, User
 from django.core.files import File
 import pandas as pd
+from django.contrib.auth.hashers import make_password
 
 
 #population script to load the users.
@@ -17,27 +18,42 @@ class Populate():
 
         path = os.path.join(os.getcwd(), 'myaccounts.csv')
         data = pd.read_csv(path)
+
         print(data)
 
-    
-    
-    
-    #def populate():
-    #restaurants = [
-     #   {'name': 'Picnic'},
-      #  {'name': 'Serenity Now'},
-       # {'name': 'Mono'},
-       # {'name': 'Hug and Pint'},
-       # {'name': 'Puti Vegan Cafe'},
-       # {'name': 'The Glasvegan'},
-       # {'name': 'The V&V Cafe'},
-       # {'name': 'The 78'} ]
+        #loop trough the rows
+        for ir in data.itertuples():
 
-#def add_restaurant(name):
- #   r = Rate.objects.get_or_create(name=name)[0]
-  #  r.save()
-   # return r
+            #Connect the path for images
+            imgpath = os.path.join(os.getcwd(), 'media', 'profile_images', str(ir[12]) + '.jpg')
 
-#if __name__=='__main__':
- #   print('Starting Veganation population script...')
-  #  populate()
+
+            #creating the user
+            user=User.objects.create(
+                username = ir[1],
+                email = ir[2],
+                password = make_password(ir[3]),
+
+            )
+
+            #create myaccount object and save data to all the fields.
+            profile = UserProfile.objects.create(user = user)
+            profile.image.save(str(ir[12]), File(open(imgpath, 'rb')))
+            profile.firstName = ir[4]
+            profile.lastName = ir[5]
+            profile.gender = ir[6]
+            profile.veganSince = ir[7]
+            profile.age = ir[8]
+            profile.quote = ir[9]
+            profile.occupation = ir[10]
+            profile.city = ir[11]
+
+            user.save()
+            profile.save()
+
+if __name__ == "__main__":
+    Populate()
+    
+    
+    
+   
